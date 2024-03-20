74760c81eec6bdba25930653cee7c4bd0f1c688d
Ksusie0214 <tdlrtmvldj@gmail.com>
2024-03-20 16:22:32 +0900

    vue

---
+++ src/views/HelloView.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>
+        Hello Vue.js
+
+    </h1>
+</template>
+
+<script>
+export default{
+    name : "HelloView",
+    data(){
+        return{
+            
+        }
+
+
+    }
+
+}
+</script>
+
+<style></style>
